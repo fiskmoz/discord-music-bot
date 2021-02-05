3fe6c146c61a21e62c3e4ef40e3431ac3e800d61
Fiskmoz <fiskmoz96@gmail.com>
2021-02-05 15:27:35 +0100

    play youtube playlists

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -33,3 +33,58 @@ export interface Song {
   title: string;
   url: string;
 }
+
+export interface YoutubeRequestConfig {
+  q?: string;
+  key: string;
+  part: string;
+  playlistId?: string;
+  maxResults?: number;
+  type: string;
+  order: string;
+}
+
+export interface YoutubeVideoResult {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: any;
+  channelTitle: string;
+  playlistId: string;
+  position: number;
+  resourceId: any;
+}
+
+export interface YouTubeSearchResults {
+  id: string;
+  link: string;
+  kind: string;
+  publishedAt: string;
+  channelTitle: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: YouTubeSearchResultThumbnails;
+}
+
+export interface YouTubeThumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface YouTubeSearchResultThumbnails {
+  default?: YouTubeThumbnail;
+  medium?: YouTubeThumbnail;
+  high?: YouTubeThumbnail;
+  standard?: YouTubeThumbnail;
+  maxres?: YouTubeThumbnail;
+}
